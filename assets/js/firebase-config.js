18de4b2ecce304e24fd10c9180183853b75ed3d1
visionedudomain <visionedudomain@github.com>
2026-03-21 15:20:03 +0530

    update made

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -2,7 +2,7 @@
   window.VisionFirebaseConfig = window.VisionFirebaseConfig || {
     apiKey: "AIzaSyC8reycExPGEuCB2M8ZyjyTX2uTq4KQh9k",
     authDomain: "vision-web-e4fc7.firebaseapp.com",
-    projectId: "vision-web-e4fc",
+    projectId: "vision-web-e4fc7",
     storageBucket: "vision-web-e4fc7.firebasestorage.app",
     messagingSenderId: "149280648043",
     appId: "1:149280648043:web:8c3f8ffb0195148f4dfa2e",
